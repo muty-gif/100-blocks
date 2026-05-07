f57fb290a2f881212a1a8fb7f872414ab20e7bf0
muty-gif <muteerafi@gmail.com>
2026-05-07 21:07:12 +0200

    Added app features

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: '100 Blocks',
+        short_name: '100 Blocks',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#111111',
+        theme_color: '#111111',
+        icons: [
+          {
+            src: '/icon.png',
+            sizes: '192x192',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
